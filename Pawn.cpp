2adb109495310431965969c9d3db356411537a61
CS Fall 19 Course DH <mgonza60@jhu.edu>
2019-11-20 17:21:26 -0500

    Pawn.cpp and King.cpp made

---
+++ Pawn.cpp
@@ -0,0 +1,14 @@
+include "Pawn.h"
+
+Pawn::Pawn(bool color){
+  white = color;
+}
+
+bool Pawn::legal_capture_shape((std::pair<char, char> start, std::pair<char, char> end)) const {
+  if (((start->first = end->first - 1) || (start->first = end->first + 1)) && (start->second == end->second -1)){
+    return true;
+  }
+  else {
+    return false;
+  }
+}

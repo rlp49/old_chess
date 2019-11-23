8ee9055510e23c9bcf9e237e64778135967ff233
Nick Jiang <njiang7@jh.edu>
2019-11-23 09:18:50 -0500

    worked on in_check

--- Chess.cpp
+++ Chess.cpp
@@ -3,6 +3,35 @@
 
 using std::cout; using std::endl;
 
+// added function itw() to check for in the way pieces
+static bool itw(pair<char, char> start, pair<char, char> end) {
+    int cFile = end->first - start->first;
+    int cRank = end->second - start->second;
+    int addF;
+    int addR;
+    
+    if (cFile < 0)
+        addF = -1;
+    else if(cFile ==0)
+        addF = 0;
+    else
+        addF = 1;
+    
+    if (cRank < 0)
+        addR = -1;
+    else if(cRank ==0)
+        addR = 0;
+    else
+        addR = 1;
+    
+    int step = 1;
+    while((step*addR != cRank) || (step*addF != cFile)){
+        pair<char, char> steps = (start->first + step*addF, start->second + step*addR);
+        if(ooc[steps])
+            return false;
+    }
+}
+
 /////////////////////////////////////
 // DO NOT MODIFY THIS FUNCTION!!!! //
 /////////////////////////////////////
@@ -39,7 +68,7 @@ Chess::Chess() : is_white_turn(true) {
 }
 
 bool Chess::make_move(std::pair<char, char> start, std::pair<char, char> end) {
-    // TODO check if start is a piece
+    // check if start is a piece
     Piece* piece = occ[start];
     if (piece == NULL)
     {
@@ -47,14 +76,14 @@ bool Chess::make_move(std::pair<char, char> start, std::pair<char, char> end) {
         return false;
     }
 
-    // TODO Check if the piece is the current player's piece 
+    // Check if the piece is the current player's piece 
     if (piece->white != turn_white())
     {
         cout << "Not your piece" << endl;
         return false;
     }
 
-    // TODO check if end is in bounds of board
+    // check if end is in bounds of board
     if (!(end->first >= 'A' && end->first <= 'H' && end->second >= '1' && end->second <= '8'))
     {
         cout << "end pos is out of bounds" << endl;
@@ -71,9 +100,9 @@ bool Chess::make_move(std::pair<char, char> start, std::pair<char, char> end) {
 
     
 
-    // TODO call valid_move for the piece
+    // call valid_move for the piece
     if(ooc[start].legal_move_shape(start, end)){
-      // TODO if not knight, check if there are no pieces in between
+        // if not knight, check if there are no pieces in between
         if(ooc[start].to_ascii != 'N' && ooc[start].to_ascii != 'n'){
             int cFile = end->first - start->first;
 	        int cRank = end->second - start->second;
@@ -110,19 +139,18 @@ bool Chess::make_move(std::pair<char, char> start, std::pair<char, char> end) {
 	        }
         }
     }
-    else if(ooc[start].to_ascii() == 'P' ||ooc.to_ascii() == 'p'){
+    else if(ooc[start].to_ascii() == 'P' || ooc.to_ascii() == 'p'){
         if (!ooc[start].legal_capture_shape(start,end)){
 	        cout << "Invalid Move" << endl;
 	        return false;
         }
     }
 
-
-    // TODO move by swapping pointer of start and end, set start to note
-
+    // move by swapping pointer of start and end, set start to note
     Piece* temp = ooc[end];
     ooc[end] = ooc[start];
     ooc[start] = NULL;
+
     // Check for if this move puts the player in check
     if(in_check(turn_white)){
       ooc[start] = ooc[end];
@@ -137,7 +165,39 @@ bool Chess::make_move(std::pair<char, char> start, std::pair<char, char> end) {
 bool Chess::in_check(bool white) const {
     // check if the king is threatened in the current board state
 
+    // TODO Direct check. Test if the king is currently in check from the opponents move.
+    pair<char, char> prev;  // game needs to somehow keep track on the previous move as a field
+    pair<char, char> king;  // also should keep track of king as a field or loop through board to find manually
     
+    // returns true if prev has a legal move to capture and no peice in the way (if not rook)
+    if (tolower(ooc[prev].to_ascii() == 'p'))
+        return ooc[prev].legal_capture_shape(prev, king) && !itw(prev, king); // if prev is a pawn
+    else if (tolower(ooc[prev].to_ascii() == 'r'))
+        return ooc[prev].legal_capture_shape(prev, king); // if prev is a rook
+    else
+        return ooc[prev].legal_capture_shape(prev, king) && !itw(prev, king); // if prev is anything else
+
+    // TODO Discovered check. Test if the king is put into check by your own move.
+    bool in_check = false;
+    
+    // looping through the entire board to chech each piece
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; i < 8; j++) {
+            if (in_check) // return if we already found piece that puts king in check
+                return true;
+
+            pair<char, char> piece = make_pair(i+'A', j+'0');
+            if (ooc[piece].is_white() != white) { // check if piece in board tho opposite player's 
+                if (tolower(ooc[piece].to_ascii() == 'p'))
+                    in_check = ooc[piece].legal_capture_shape(piece, king) && !itw(piece, king);
+                else if (tolower(ooc[piece].to_ascii() == 'r'))
+                    in_check = ooc[piece].legal_capture_shape(piece, king);
+                else
+                    in_check = ooc[piece].legal_capture_shape(piece, king) && !itw(piece, king); 
+            }
+        }
+    }
+                    
 	return false;
 }
 

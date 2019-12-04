69aa70e16ab48b47fed18b8b6ed993d446071c94
jlee664 <jlee664@jh.edu>
2019-12-04 15:36:00 -0500

    Mystery piece testing

--- Mystery.h
+++ Mystery.h
@@ -1,7 +1,3 @@
-///////////////////////////////////
-// IT IS OK TO MODIFY THIS FILE, //
-// YOU WON'T HAND IT IN!!!!!     //
-///////////////////////////////////
 #ifndef MYSTERY_H
 #define MYSTERY_H
 
@@ -9,19 +5,34 @@
 
 class Mystery : public Piece {
 
+//moves in 3x2; think knight but bigger
 public:
   virtual ~Mystery(){}
 	bool legal_move_shape(std::pair<char, char> start, std::pair<char, char> end) const {
-		(void) start;//meaningless statement helps avoid unused parameter warning for now
-		(void) end;  //meaningless statement helps avoid unused parameter warning for now
+		if((end.first == start.first + 3 && 
+				(end.second == start.second + 2 || end.second == start.second - 2)) ||
+			 (end.first == start.first - 3 &&
+				(end.second == start.second + 2 || end.second == start.second - 2)) ||
+			 (end.second == start.second + 3 &&
+				(end.first == start.first + 2 || end.first == start.first - 2)) ||
+			 (end.second == start.second - 3 &&
+				(end.first == start.first + 2 || end .first == start.first - 2))) {
+			return true;
+		}
 		return false;
 	}
-
+	
+	/////////////////////////////////////
+	// DO NOT MODIFY THIS FUNCTION!!!! //
+	/////////////////////////////////////
 	char to_ascii() const {
 		return is_white() ? 'M' : 'm';
 	}
 
 private:
+	/////////////////////////////////////
+	// DO NOT MODIFY THIS FUNCTION!!!! //
+	/////////////////////////////////////
 	Mystery(bool is_white) : Piece(is_white) {}
 
 	friend Piece* create_piece(char piece_designator);

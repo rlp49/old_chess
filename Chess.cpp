3a083ddeacaf1d031cbb429fda5f8203538ea7b7
jlee664 <jlee664@jh.edu>
2019-12-04 17:32:42 -0500

    Chess.cpp cleanup

--- Chess.cpp
+++ Chess.cpp
@@ -10,11 +10,12 @@ using std::vector;
 
 // overloaded itw()
 bool Chess::itw(pair<char, char> start, pair<char, char> end, Board& board) const {
-  int cFile = end.first - start.first;
-  int cRank = end.second - start.second;
+  int cFile = end.first - start.first; //how many files ahead the end pos is
+  int cRank = end.second - start.second; //how many ranks ahead the end pos is
   int addF;
   int addR;
   
+	//jumping piece, ignore itw
   if((abs(cFile) != abs(cRank)) && cFile != 0 && cRank != 0){
     return false;
   }
@@ -33,6 +34,7 @@ bool Chess::itw(pair<char, char> start, pair<char, char> end, Board& board) cons
   else
     addR = 1;
   
+	//checks if there's a piece in each pos b/w start and end
   pair<char, char> steps = make_pair(start.first + addF, start.second + addR);
   while(!(steps.first == end.first && steps.second == end.second)) {
     if(board(steps) != nullptr) return true;
@@ -45,11 +47,12 @@ bool Chess::itw(pair<char, char> start, pair<char, char> end, Board& board) cons
 
 // added function itw() to check for in the way pieces
 bool Chess::itw(pair<char, char> start, pair<char, char> end) const {
-  int cFile = end.first - start.first;
-  int cRank = end.second - start.second;
+  int cFile = end.first - start.first; //how many files ahead the end pos is
+  int cRank = end.second - start.second; //how many ranks ahead the end pos is
   int addF;
   int addR;
   
+	//jumping piece, ignore itw
   if((abs(cFile) != abs(cRank)) && cFile != 0 && cRank != 0){
     return false;
   }
@@ -68,6 +71,7 @@ bool Chess::itw(pair<char, char> start, pair<char, char> end) const {
   else
     addR = 1;
   
+	//checks each square b/w start and end for itw
   pair<char, char> steps = make_pair(start.first + addF, start.second + addR);
   while(!(steps.first == end.first && steps.second == end.second)) {
     if(board(steps) != nullptr) return true;
@@ -118,90 +122,83 @@ bool Chess::make_move(std::pair<char, char> start, std::pair<char, char> end) {
   // check if start is a piece
   const Piece* piece = board(start);
     bool othph = false;
-    if (piece == nullptr)
-      {
+    if (piece == nullptr) {
       cout << "No piece at starting pos" << endl;
-        return false;
+      return false;
     }
     
     // Check if the piece is the current player's piece 
-    if (piece->is_white() != turn_white())
-      {
-	cout << "Not your piece" << endl;
+    if (piece->is_white() != turn_white()) {
+			cout << "Not your piece" << endl;
       return false;
     }
     
     // check if end is in bounds of board
-    if (!(end.first >= 'A' && end.first <= 'H' && end.second >= '1' && end.second <= '8'))
-      {
-	cout << "end pos is out of bounds" << endl;
+    if (!(end.first >= 'A' && end.first <= 'H' && end.second >= '1' && end.second <= '8')) {
+			cout << "end pos is out of bounds" << endl;
         return false;
-      }
-    
-    //Check if Piece at end is of the same color
-    if (board(end) != nullptr){
-      if (piece->is_white() == board(end)->is_white()){
-	cout << "Your piece occupies that square" << endl;
-	return false;
-      }
-      else{
-	othph = true;
-      }
     }
     
-    bool pawnall = true;
-    if (tolower(piece->to_ascii()) == 'p'){
-      if(othph){
-	pawnall = false;
+    //Check if Piece at end is of the same color
+    if (board(end) != nullptr) {
+      if (piece->is_white() == board(end)->is_white()) {
+				cout << "Your piece occupies that square" << endl;
+				return false;
       }
+      else
+				othph = true;
+    }
+    
+		// pawnall true if capture possible, but move not possible
+    bool pawnall = true;
+    if (tolower(piece->to_ascii()) == 'p') {
+      if(othph)
+				pawnall = false;
     }
     
     // call valid_move for the piece
-    if(piece->legal_move_shape(start, end) && pawnall){
-      // if not knight, check if there are no pieces in between
-      if(tolower(piece->to_ascii()) != 'n'){
-	if (itw(start, end)) {
-	  cout << "Piece in the way" << endl;
-	  return false; // if piece is in the way, return false
-	}
-      }
+    if(piece->legal_move_shape(start, end) && pawnall) {
+      // Check if there are no pieces in between
+			if(itw(start, end)) {
+	  		cout << "Piece in the way" << endl;
+	  		return false; // if piece is in the way, return false
+			}
     }
-    else if(tolower(piece->to_ascii()) == 'p' && !pawnall){
-      std::cout<< "Here" << std::endl;
-      if((!(piece->legal_capture_shape(start,end)))){
-	std::cout<< "Here2" << std::endl;
-	
-	cout << "Invalid Move" << endl;
-	  return false;
+    else if(tolower(piece->to_ascii()) == 'p' && !pawnall) {
+      if((!(piece->legal_capture_shape(start,end)))) 	
+				cout << "Invalid Move" << endl;
+	  	return false;
       }
     }
     else {
       cout << "Invalid Move" << endl;
       return false;
     }
+	
     // make the move
     Board board_old = board; // make a copy of old board in case we need to undo the move, make sure it has a copy constructor
     board.move_piece(start, end);
     
     // Check for if this move puts the player in check
-    if(in_check(turn_white())){
+    if(in_check(turn_white())) {
       board = board_old; // revert to old board
-        cout<< "That puts you in check"<<endl;
-        return false;
+      cout << "That puts you in check" << endl;
+      return false;
     }
  
-  // pawn promotion
+  	// pawn promotion
     if(tolower(piece->to_ascii()) == 'p') {
-      if(piece->is_white() && end.second == '8'){
-	board.remove_piece(end);
-	board.add_piece(end,'Q');
+      if(piece->is_white() && end.second == '8') {
+				board.remove_piece(end);
+				board.add_piece(end,'Q');
       }
-      if(!piece->is_white() && end.second == '1'){
-	board.remove_piece(end);
-	board.add_piece(end,'q');
+      if(!piece->is_white() && end.second == '1') {
+				board.remove_piece(end);
+				board.add_piece(end,'q');
       }
     }
     
+		//changes turn
     is_white_turn = !is_white_turn;
     return true;
 }
@@ -215,173 +212,134 @@ bool Chess::make_move(std::pair<char, char> start, std::pair<char, char> end, Bo
     return false;
   }
     
-    // Check if the piece is the current player's piece 
+  // Check if the piece is the current player's piece 
   if (piece->is_white() != turn_white()) {
     return false;
   }
     
-    // check if end is in bounds of board
+  // check if end is in bounds of board
   if (!(end.first >= 'A' && end.first <= 'H' && end.second >= '1' && end.second <= '8')) {
     return false;
   }
     
-    //Check if Piece at end is of the same color
-    if (board(end) != nullptr){
-      if (piece->is_white() == board(end)->is_white()){
-	return false;
-      }
-      else{
-	othph = true;
-      }
-    }
+  //Check if Piece at end is of the same color
+  if (board(end) != nullptr) {
+    if (piece->is_white() == board(end)->is_white())
+		  return false;
+    else 
+	  	othph = true;
+  }
     
-    bool pawnall = true;
-    if (tolower(piece->to_ascii()) == 'p'){
-      if(othph){
-	pawnall = false;
-      }
-    }
+  bool pawnall = true;
+  if (tolower(piece->to_ascii()) == 'p') {
+    if(othph)
+			pawnall = false;
+  }
     
-    // call valid_move for the piece
-    if(piece->legal_move_shape(start, end) && pawnall){
-      // if not knight, check if there are no pieces in between
-      if(tolower(piece->to_ascii()) != 'n'){
-	if (itw(start, end, board)) {
-	  return false; // if piece is in the way, return false
-	}
-      }
-    }
-    else if(tolower(piece->to_ascii()) == 'p' && !pawnall){
-      if
-      (!(piece->legal_capture_shape(start,end))){
-	  return false;
-      }
-    }
-    else {
-      return false;
-    }
-    // make the move
-    board.move_piece(start, end);
+  // call valid_move for the piece
+  if(piece->legal_move_shape(start, end) && pawnall) {
+	  if(itw(start, end, board)) 
+	  	return false; // if piece is in the way, return false
+  }
+  else if(tolower(piece->to_ascii()) == 'p' && !pawnall) {
+      if(!(piece->legal_capture_shape(start,end)))
+	  		return false;
+  }
+  else 
+    return false;
+ 
+  // make the move
+  board.move_piece(start, end);
     
-    // Check for if this move puts the player in check
-    if(in_check(turn_white(), board)){
-        return false;
-    }
+  // Check for if this move puts the player in check
+  if(in_check(turn_white(), board))
+    return false;   
     
-    if(tolower(piece->to_ascii()) == 'p') {
-      if(piece->is_white() && end.second == '8'){
-	board.remove_piece(end);
-	board.add_piece(end,'Q');
-      }
-      if(!piece->is_white() && end.second == '1'){
-	board.remove_piece(end);
-	board.add_piece(end,'q');
-      }
+  //pawn promotion
+  if(tolower(piece->to_ascii()) == 'p') {
+    if(piece->is_white() && end.second == '8') {
+			board.remove_piece(end);
+			board.add_piece(end,'Q');
     }
+    if(!piece->is_white() && end.second == '1') {
+			board.remove_piece(end);
+			board.add_piece(end,'q');     
+		}
+  }
     
-    return true;
+  return true;
 }
 
 bool Chess::in_check(bool white) const {
-    pair<char, char> king;  
-    for (char i = 'A'; i <= 'H'; i++) {
-      for (char j = '1'; j <= '8'; j++) {
-        pair<char,char> piece = make_pair(i,j);
-        if (board(piece) == nullptr) {
-          continue;
-        }
-        if (board(piece)->to_ascii() == 'K' && white) {
-          king = piece;
-          break;
-        }
-        if (board(piece)->to_ascii() == 'k' && !white) {
-          king = piece;
-          break;
-        }
+	//finds location of the king
+  pair<char, char> king;  
+  for(char i = 'A'; i <= 'H'; i++) {
+    for(char j = '1'; j <= '8'; j++) {
+      pair<char,char> piece = make_pair(i,j);
+      if(board(piece) == nullptr) 
+        continue;
+      if(board(piece)->to_ascii() == 'K' && white) {
+        king = piece;
+        break;
+      }
+      if (board(piece)->to_ascii() == 'k' && !white) {
+        king = piece;
+        break;
       }
     }
-    //cout << "location of king: " << king.first << king.second << endl; 
-    // looping through the entire board to chech each piece
-    for (char i = 'A'; i <= 'H'; i++) {
-        for (char j = '1'; j <= '8'; j++) {
-	        pair<char, char> piece = make_pair(i, j);
-            //cout << "checking pair: " << piece.first << piece.second << endl; 
-            if (board(piece) == nullptr) {
-                continue; // pass if piece doesn't exist
-            }
-
-            if (board(piece)->is_white() == white || !board(piece)->is_white() == !white) {
-                continue; // pass if piece is same color as the king
-            }
-
-        // only knight's can jump over other pieces so no need to check for in the way pieces
-            if (tolower(board(piece)->to_ascii() == 'n')) { 
-              if (board(piece)->legal_capture_shape(piece, king)) {
-                cout << "put in check by: " << piece.first << piece.second << endl;
-                return true;
-              }
-            }
-        // for all other pieces
-            if(board(piece)->legal_capture_shape(piece, king) && !itw(piece, king)) {
-                cout << "put in check by: " << piece.first << piece.second << endl;
-                return true; 
-            }
-
-            //cout << "piece: " << board(piece)->to_ascii() << " does not threaten king" << endl;
-        }
+  }
+   
+  // looping through the entire board to check each piece
+  for(char i = 'A'; i <= 'H'; i++) {
+    for(char j = '1'; j <= '8'; j++) {
+	    pair<char, char> piece = make_pair(i, j);
+      if(board(piece) == nullptr) 
+        continue; // pass if piece doesn't exist
+      if(board(piece)->is_white() == white || !board(piece)->is_white() == !white) 
+        continue; // pass if piece is same color as the king
+      // checks if king in check
+      if(board(piece)->legal_capture_shape(piece, king) && !itw(piece, king)) {
+        cout << "put in check by: " << piece.first << piece.second << endl
+        return true; 
+  		}
     }
-    return false;
+  }
+  return false;
 }
 
 // overloaded in_check()
-bool Chess::in_check(bool white, Board& board) const {
-   //cout << "running overloaded in_check()" << endl; 
-    pair<char, char> king;  
-    // loop to find the king
-    for (char i = 'A'; i <= 'H'; i++) {
-      for (char j = '1'; j <= '8'; j++) {
-        pair<char,char> piece = make_pair(i,j);
-        if (board(piece) == nullptr)
-          continue;
-        if (board(piece)->to_ascii() == 'K' && white) {
-          king = piece;
-          break;
-        }
-        if (board(piece)->to_ascii() == 'k' && !white) {
-          king = piece;
-          break;
-        }
+bool Chess::in_check(bool white, Board& board) const { 
+  pair<char, char> king;  
+  // loop to find the king
+  for(char i = 'A'; i <= 'H'; i++) {
+    for(char j = '1'; j <= '8'; j++) {
+      pair<char,char> piece = make_pair(i,j);
+      if(board(piece) == nullptr)
+        continue;
+      if(board(piece)->to_ascii() == 'K' && white) {
+        king = piece;
+        break;
+      }
+      if(board(piece)->to_ascii() == 'k' && !white) {
+        king = piece;
+        break;
       }
     }
-
-    // see if any opponent pieces can capture king
-    for (char i = 'A'; i <= 'H'; i++) {
-        for (char j = '1'; j <= '8'; j++) {
-	        pair<char, char> piece = make_pair(i, j);
-            if (board(piece) == nullptr) {
-                continue; // pass if piece doesn't exist
-            }
-
-            if (board(piece)->is_white() == white || !board(piece)->is_white() == !white) {
-                continue; // pass if piece is same color as the king
-            }
-
-        // only knight's can jump over other pieces so no need to check for in the way pieces
-            if (tolower(board(piece)->to_ascii() == 'n') && 
-                board(piece)->legal_capture_shape(piece, king)) {
-                //cout << "put in check by: " << piece.first << piece.second << endl;
-                return true;
-            }
-        
-        // for all other pieces
-            if(board(piece)->legal_capture_shape(piece, king) && !itw(piece, king, board)) {
-                //cout << "put in check by: " << piece.first << piece.second << endl;
-                return true; 
-            }
-
-        }
+  }
+  // see if any opponent pieces can capture king
+  for(char i = 'A'; i <= 'H'; i++) {
+    for(char j = '1'; j <= '8'; j++) {
+	    pair<char, char> piece = make_pair(i, j);
+      if(board(piece) == nullptr) 
+        continue; // pass if piece doesn't exist
+      if(board(piece)->is_white() == white || !board(piece)->is_white() == !white)
+        continue; // pass if piece is same color as the king
+      // checks if king is in check
+      if(board(piece)->legal_capture_shape(piece, king) && !itw(piece, king, board)) 
+        return true; 
     }
-    return false;
+  }
+  return false;
 }
 
 // checks for legal moves for all pieces, returns true if no legal moves found
@@ -390,31 +348,28 @@ bool Chess::in_stalemate(bool white) const {
   vector<pair<char,char>> pieces; // vector storing pieces to check moves with
   
   // first for loop gets a vector of pieces
-  for (char i = 'A'; i <= 'H'; i++) {
-    for (char j = '1'; j <= '8'; j++) {
+  for(char i = 'A'; i <= 'H'; i++) {
+    for(char j = '1'; j <= '8'; j++) {
       pair<char,char> piece = make_pair(i,j);
-      
-      if (this->board(piece) == nullptr)
+      if(this->board(piece) == nullptr)
         continue;
-      
+     
       // check if same color
-      if (this->board(piece)->is_white() == white || !this->board(piece)->is_white() == !white)
+      if(this->board(piece)->is_white() == white || !this->board(piece)->is_white() == !white)
         pieces.push_back(piece);
     }
   }
 
   // second for loop makes moves with the pieces in vector
-  for (unsigned i = 0; i < pieces.size(); i++) {
-    pair<char,char> cur = pieces[i]; // current piece to check moves with
-    //cout << "checking piece " << cur.first << cur.second << endl;    
+  for(unsigned i = 0; i < pieces.size(); i++) {
+    pair<char,char> cur = pieces[i]; // current piece to check moves with    
     for (char j = 'A'; j <= 'H'; j++) {
       for (char k = '1'; k <= '8'; k++) {
         pair<char,char> move = make_pair(j,k); // create a possible move
         Board old = board; // make a copy of board to undo the move
-        if (make_move(cur, move, board)) {
-          return false; // if move was succesful and not in check anymore
-        }
-        board = old; // need asignment operator
+        if (make_move(cur, move, board)) 
+          return false; // if move was succesful and not in check anymore      
+        board = old; // need assignment operator
       }
     }
   }
@@ -436,7 +391,6 @@ std::ostream& operator<< (std::ostream& os, const Chess& chess) {
 }
 
 std::istream& operator>> (std::istream& is, Chess& chess) {
-
   //clearing current board
   std::pair<char, char> coord;
   for(char i = 'A'; i <= 'H'; i++) {
@@ -450,14 +404,12 @@ std::istream& operator>> (std::istream& is, Chess& chess) {
   for(char k = '8'; k >= '1'; k--) {
     for(char l = 'A'; l <= 'H'; l++) {
       is.get(c);
-      //std::cout << c;
       coord = make_pair(l, k);
       if(c=='p'||c=='P'||c=='k'||c=='K'||c=='n'||c=='N'||c=='q'||c=='Q'||c=='r'||c=='R'||c=='b'||c=='B'||c=='M'||c=='m') {
         chess.board.add_piece(coord, c);
       }
     }
     is.get(c);
-    //std::cout << c;
   }
   is.get(c);
   if(c == 'w') 
